e70a7cd8b6ea26ddf1432b673b8ba06cef4f8805
Jpbenjamim <dev.bejamim@gmail.com>
2022-07-14 10:58:19 -0300

    update navbar

--- js/main.js
+++ js/main.js
@@ -324,7 +324,7 @@
 
 function makeTimer() {
 
-		var endTime = new Date("21 December 2019 9:56:00 GMT+01:00");			
+		var endTime = new Date("31 jully 2022 23:59:59 GMT-03:00");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
